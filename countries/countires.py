97eef6862427fe2b0f4e8be5806c388c29fee8fa
JackCT1 <jackthomas933@gmail.com>
2023-10-10 21:23:16 +0100

    displaying country data

--- countries/countires.py
+++ countries/countires.py
@@ -1,5 +1,55 @@
 import requests
 
+"""
 response = requests.get("https://restcountries.com/v3.1/name/germany")
 json = response.json()
-print(json)
+print(json[0]["name"]["nativeName"]["deu"]["official"])
+"""
+
+
+class APIError(Exception):
+    """Describes an error triggered by a failing API call."""
+
+    def __init__(self, message: str, code: int = 500):
+        """Creates a new APIError instance."""
+        self.message = message
+        self.code = code
+
+
+def print_data(country_data: dict):
+    """Displays country data from a dict."""
+    print(f'Country: {country_data[0]["name"]["common"]}')
+    print(f'Capital: {country_data[0]["capital"][0]}')
+    print(f'Currency: {country_data[0]["currencies"]}')
+
+
+def fetch_data(country_name: str) -> dict:
+    """Returns a dict of country data from the API."""
+    response = requests.get(f"https://restcountries.com/v3.1/name/{country_name}")
+    json = response.json()
+    return json
+
+
+def main():
+    """Repeatedly prompts the user for country names and displays the result."""
+    print(" ")
+    print("####################")
+    print("Welcome to the REST Countries Searcher")
+    print("####################")
+    print(" ")
+
+    while 1:
+        entry = input("Search for a country: ")
+        print(f"You searched for: {entry}")
+        print("Fetching...")
+        print(" ")
+        try:
+            country_data = fetch_data(entry)
+            print_data(country_data)
+        except APIError as e:
+            print(e.message)
+        print(" ")
+
+
+if __name__ == "__main__":
+    main()
